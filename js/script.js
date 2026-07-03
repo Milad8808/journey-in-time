e4fff1a0850db9826cb79d31d3a12e5d3143f9cb
Milad8808 <113549166+Milad8808@users.noreply.github.com>
2026-07-03 10:58:31 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+window.onload = () => {
 
+    const text = document.querySelector(".hero-text");
+
+    text.style.opacity = "0";
+
+    text.style.transition = "opacity 2s ease";
+
+    setTimeout(() => {
+
+        text.style.opacity = "1";
+
+    }, 800);
+
+};
